13003513f2d8d1f69093ddc99d0718c658840fe0
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2025-05-01 13:33:12 +0100

    Create main.go

---
+++ SNOWFLAKE_LAMBA/CreateStream/main.go
@@ -0,0 +1,14 @@
+package main
+func main() {
+	user := 
+	password := 
+	account := 
+
+	//CREATE STREAM
+	CreateStream(user1, password1, account1,)
+  CreateStage(user1, password1, account1,)
+	CreateView(user1, password1, account1, )
+	CreateProcedure(user1, password1, account1, )
+	RunProcedure(user1, password1, account1, )
+	CreatePreventStale(user1, password1, account1,)
+}
